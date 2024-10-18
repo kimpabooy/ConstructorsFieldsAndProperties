ed0c37e83234b64dbebb1ff2b24f27865f131b10
Kim Andersson <Kimpabooy@live.com>
2024-10-18 13:50:41 +0200

    book class done

--- Övningar - Constructors, Fields & Properties/Program.cs
+++ Övningar - Constructors, Fields & Properties/Program.cs
@@ -4,8 +4,13 @@
     {
         static void Main(string[] args)
         {
-            var bok1 = new Bok();
-
+            var bok1 = new Bok("Sagan om ringen", "Tolkien", 267 );
+            var bok2 = new Bok("Bamse", "Rune Andréasson", 20 );
+            var bok3 = new Bok("hej", "Toen", 167 );
+            
+            Console.WriteLine($"bok 1, Titel: {bok1.Titel} Författare: {bok1.Författare} Sidantal: {bok1.Sidantal}\n");
+            Console.WriteLine($"bok 2, Titel: {bok2.Titel} Författare: {bok2.Författare} Sidantal: {bok2.Sidantal}\n");
+            Console.WriteLine($"bok 3, Titel: {bok3.Titel} Författare: {bok3.Författare} Sidantal: {bok3.Sidantal}\n");
 
             Console.ReadKey();
         }
