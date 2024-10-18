9e11786a93380775054d7b44bc1b1f59c8d2d63e
Kim Andersson <Kimpabooy@live.com>
2024-10-18 12:27:43 +0200

    Add project files.

---
+++ Övningar - Constructors, Fields & Properties/Bok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övningar___Constructors__Fields___Properties
+{
+    internal class Bok
+    {
+        private string _titel;
+        private string _författare;
+        private int _sidantal;
+
+        public Bok(string titel, string författare, int sidantal)
+        {
+            _titel = titel;
+            _författare = författare;
+            _sidantal = sidantal;
+        }
+
+    }
+}
---
+++ Övningar - Constructors, Fields & Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Övningar___Constructors__Fields___Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bok1 = new Bok();
+
+
+            Console.ReadKey();
+        }
+    }
+}
